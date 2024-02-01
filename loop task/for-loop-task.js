0bd15e2ed54b9e4dc95a6aed26b0e36c5b75d7eb
shahinexy <shahinexy@gmail.com>
2024-02-01 18:59:38 +0600

    loop task folder and for loop task added

---
+++ loop task/for-loop-task.js
@@ -0,0 +1,53 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+
+
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 91 to 129.
+
+ */
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 51 to 85.
+
+ */
+
+/***
+
+Generate a multiplication table for number 9
+
+ */
+
+
+/***
+
+Implement a countdown timer that counts down from 81 to 65.
+
+ */

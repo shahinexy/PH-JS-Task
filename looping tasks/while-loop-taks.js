5fa9a34474b4c2442db813a72192ce148e3805fd
shahinexy <shahinexy@gmail.com>
2024-02-01 23:27:48 +0600

    while loop tasks answer completed

---
+++ looping tasks/while-loop-taks.js
@@ -0,0 +1,24 @@
+/*
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+let i = 0;
+while(i <= 60){
+    console.log('I will invest at last 6 hrs every single day for next 60 day!');
+    i++;
+}
+
+/*
+Subtask-1:
+
+Display sum of all the odd numbers from 91 to 129.
+
+ */
+
+let odd = 91;
+while (odd <= 129) {
+    if(odd % 2 !== 0){
+        console.log(odd);
+    }
+    odd++;
+}

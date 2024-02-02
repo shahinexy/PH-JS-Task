427e19048af548a984fceb3a1d10a25f1b6e2bb3
shahinexy <shahinexy@gmail.com>
2024-02-02 11:30:17 +0600

    extra tasks added

--- looping tasks/extra-tasks.js
+++ looping tasks/extra-tasks.js
@@ -10,3 +10,18 @@ for(let i =  fruitsRevarse.length - 1; i >= 0; i--){
     console.log(fruitsRevarse[i]);
 }
 console.log('===================');
+
+// sum form an array 
+const arraySum = [10, 20 ,30];
+let  sumOFArray = 0;
+for(let i = 0; i < arraySum.length; i++){
+    sumOFArray += arraySum[i];
+}
+console.log(sumOFArray);
+
+// swap array element 
+const swapArray = [1,2,3,4,5];
+let swap = swapArray[3];
+swapArray[3] = swapArray[1] 
+swapArray[1] = swap;
+console.log(swapArray);

1116c610170386994cb6cee32ef35968588bf624
shahinexy <shahinexy@gmail.com>
2024-02-02 12:30:54 +0600

    more extra tasks added

--- looping tasks/extra-tasks.js
+++ looping tasks/extra-tasks.js
@@ -25,3 +25,65 @@ let swap = swapArray[3];
 swapArray[3] = swapArray[1] 
 swapArray[1] = swap;
 console.log(swapArray);
+console.log('===============');
+
+// find array element index number 
+const indexArray = ['mango', 'banana', 'apple', 'coconat'];
+const target = 'apple';
+let foundIndex = -1;
+for(let i =0; i < indexArray.length; i++){
+    const currentIndex = indexArray[i]
+    if(currentIndex === target){
+        foundIndex = i;
+    }
+}
+if(foundIndex > -1){
+    console.log('The array element index is:', foundIndex);
+}
+else{
+    console.log('Not Found');
+}
+
+
+//  Find max / min number from an array 
+const maxMin = [20,60,30,22,10,40];
+let max = maxMin[0];
+let min = maxMin[0];
+for(let i = 0; i < maxMin.length; i++){
+    let current = maxMin[i];
+    if(max < current){
+        max = current;
+    }
+    if( min > current){
+        min = current;
+    }
+}
+console.log('maximum number is:', max, 'and the minimum number is:', min); 
+console.log('================');
+
+// Filter in an array 
+const filterArray = [20,60,30,22,10,40];
+let filterResult = [];
+for(let i = 0; i < filterArray.length; i++){
+    current = filterArray[i];
+    if(current <= 30){
+        filterResult.push(current);
+    }
+}
+console.log(filterResult);
+console.log('==============');
+
+// filter and give discount 
+const discountArray = [20,60,30,22,10,40];
+let discount = [];
+for(let i = 0; i < discountArray.length; i++){
+    current = discountArray[i];
+    if(current >= 30){
+        let calculetDiscount = current - current * 10 / 100;
+        discount.push(calculetDiscount);
+    }
+    else{
+        discount.push(current)
+    }
+}
+console.log(discount);

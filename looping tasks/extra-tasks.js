b43d7a9e85c4ce4df21ae7b4d8ba849dfe85c962
shahinexy <shahinexy@gmail.com>
2024-02-02 10:42:58 +0600

    extra tasks folder added

---
+++ looping tasks/extra-tasks.js
@@ -0,0 +1,12 @@
+const fruits = ['mango', 'banana', 'aple', 'coconat'];
+for(let i =0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
+console.log('===================');
+
+// revarse array 
+const fruitsRevarse = ['mango', 'banana', 'aple', 'coconat'];
+for(let i =  fruitsRevarse.length - 1; i >= 0; i--){
+    console.log(fruitsRevarse[i]);
+}
+console.log('===================');

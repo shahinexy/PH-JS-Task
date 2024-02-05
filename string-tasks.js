e44fba76b16c48daad79574e42d610738b1bb9d6
shahinexy <shahinexy@gmail.com>
2024-02-05 10:09:55 +0600

    another task added

--- string-tasks.js
+++ string-tasks.js
@@ -9,11 +9,30 @@ for (let i = 0; i < split.length; i++) {
     }
 }
 console.log(`This string contain a ${countA} times`);
+console.log('=======================================');
+
 
 // Count how many times a string has the letter a or A
+const str2 = "I A am A learning A web development. Now i am learning about string AA";
+const split2 = str2.split('')
+console.log(split2);
+let countAa = 0;
+for (let i = 0; i < split2.length; i++) {
+    let index = split2[i]
+    if(index.includes('a')){
+        countAa += 1;
+    }
+    else if(index.includes('A')){
+        countAa += 1;
+    }
+}
+console.log(`This string contain a or A ${countAa} times`);
+console.log('=======================================');
 
 
 // Check whether a string contains all the vowels a, e, i, o, u
+const checkVowels = 'I am learning web development. Now i am learning about string';
+
 
 // If a given string has either x, replace x by y. if the given string has X, replace it by Y.
 // Hint: You should be able to check whether x or X exists. After that, search online how to replace a character in a string.

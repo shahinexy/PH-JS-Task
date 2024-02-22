c7a91bda5fe60bad7dd7e2be1fe1fee9a1f4f694
shahinexy <shahinexy@gmail.com>
2024-02-22 13:24:06 +0600

    all question added

---
+++ ES6 Task/provlem-solving.js
@@ -0,0 +1,17 @@
+// (1) write an arrow function that will take 3 parameters, will multiply the parameter and return the result
+
+
+// (2) Write the following sentence in three line and print the result: I am a web developer. i love to code. I love to eat biryani.
+
+
+// (3) Write an arrow function that will take 2 paramenter. One parameter will come from you and ther other will be a default parameter. add these two parameter and return the result.
+
+// (4) Write an arrow function where it will fo the following:
+// a) It will take an array where the array element will be the name of your friends.
+// b) Check if the length of each element is even, push elements with even length to a new array and return the result.
+
+
+// (5) Write an arrow function where it will do the folloing:
+// a) Square each array element.
+// b) Calculate the sum of the aquare element.
+// c) Return the average of the squared element.

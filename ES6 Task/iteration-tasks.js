d1d3eaf0fdcd988276828637a1d4af2fb0feb88b
shahinexy <shahinexy@gmail.com>
2024-02-25 13:04:57 +0600

    some answer added

--- ES6 Task/iteration-tasks.js
+++ ES6 Task/iteration-tasks.js
@@ -1,8 +1,13 @@
 // (1) You have an odd array [1,3,5,7,9]. Now convert this array in to an even array using array.map().
-
+const myArr = [1, 3, 5, 7, 9];
+const evenArr = myArr.map(number => number + 1);
+console.log(evenArr);
+console.log('=====================');
 
 // (2) You are given an array say: [33,50,79,90,101,30]. Now return/get all the element which are divisible by 10 using array.filter() method.
-
+const myArr2 = [33, 50, 79, 90, 101, 30];
+const divisible = myArr2.filter(number => number % 10 == 0);
+console.log(divisible);
 
 // (3) You have an array of object:
 // const instructor = [
@@ -11,6 +16,7 @@
 //     {name : 'Shobuj', age: 30, position: 'Senior'},
 // ]
 // you task is to display the instruction names that has the position senior using array filter.
+
 
 
 // (4)

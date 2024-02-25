c172190c03c7d4d0799ae84a0c909421e1f48a77
shahinexy <shahinexy@gmail.com>
2024-02-25 12:59:58 +0600

    array iteration task added

---
+++ ES6 Task/iteration-tasks.js
@@ -0,0 +1,22 @@
+// (1) You have an odd array [1,3,5,7,9]. Now convert this array in to an even array using array.map().
+
+
+// (2) You are given an array say: [33,50,79,90,101,30]. Now return/get all the element which are divisible by 10 using array.filter() method.
+
+
+// (3) You have an array of object:
+// const instructor = [
+//     {name : 'Nodi', age: 28, position: 'Senior'},
+//     {name : 'Akil', age: 26, position: 'Junior'},
+//     {name : 'Shobuj', age: 30, position: 'Senior'},
+// ]
+// you task is to display the instruction names that has the position senior using array filter.
+
+
+// (4)
+// const people = [
+//     {name: 'Meena', age: 20},
+//     {name: 'Rina', age: 15},
+//     {name: 'Suchorita', age: 22},
+// ];
+// You have 3 object as array element, can you find the total sum from here. use array.reduce() to do this task. sume of age is 57;
